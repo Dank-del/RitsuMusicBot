8c4018b2e950993a3a33ebce6996c8e21dbc51df
Sayan Biswas <sayan@pokurt.me>
2021-08-29 10:58:30 +0530

    start: show different messages for pm and group.
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- handlers/start.go
+++ handlers/start.go
@@ -10,9 +10,15 @@ import (
 func startHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 	msg := ctx.Message
 	user := msg.From
-
-	message := fmt.Sprintf("<b>Hi %s, I'm %s</b>!\n<i>Run /help to learn more!</i>", html.EscapeString(user.FirstName), html.EscapeString(b.FirstName))
-
+	chat := msg.Chat
+	var message string
+	if chat.Type == "private" {
+		message = fmt.Sprintf("<b>Hi %s, I'm %s</b>!\n<i>Run /help to learn more!</i>",
+			html.EscapeString(user.FirstName),
+			html.EscapeString(b.FirstName))
+	} else {
+		message = fmt.Sprintf("Hi, I'm %s, ready to show off your music", b.FirstName)
+	}
 	_, err := msg.Reply(b, message, &gotgbot.SendMessageOpts{ParseMode: "html"})
 	if err != nil {
 		return err
