68c355c55e01f711358b9d5d7575da6bf10232a4
Sayan Biswas <sayan@pokurt.me>
2021-09-02 16:42:16 +0530

    lyrics: initial inline support.
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- handlers/lyrics.go
+++ handlers/lyrics.go
@@ -9,6 +9,66 @@ import (
 	"strings"
 )
 
+func lyricsInlineFilter(q *gotgbot.InlineQuery) bool {
+	if q == nil {
+		return false
+	}
+	return strings.Contains(strings.ToLower(q.Query), lyricsCommand)
+}
+
+func lyricsInline(b *gotgbot.Bot, ctx *ext.Context) (err error) {
+	inlq := ctx.InlineQuery
+	q := strings.Trim(inlq.Query, lyricsCommand)
+	var results []gotgbot.InlineQueryResult
+	if q == "" {
+		results = append(results, gotgbot.InlineQueryResultArticle{Id: inlq.Id, Title: "Query required",
+			InputMessageContent: gotgbot.InputTextMessageContent{MessageText: "No query provided"}})
+		_, err := inlq.Answer(b,
+			results,
+			&gotgbot.AnswerInlineQueryOpts{IsPersonal: true})
+		if err != nil {
+			return err
+		}
+	}
+	var l []string
+	e := 0
+	txt := mdparser.GetBold(fmt.Sprintf("Results for %s", q)).AppendNormal("\n\n")
+	for len(l) < 5 {
+		l, err = genius.GetLyrics(q)
+		if err != nil {
+			return err
+		}
+		e++
+		if e > 10 {
+			results = append(results, gotgbot.InlineQueryResultArticle{Id: inlq.Id, Title: "Error",
+				InputMessageContent: gotgbot.InputTextMessageContent{MessageText: mdparser.GetItalic(fmt.Sprintf("Error: %s", err.Error())).ToString(),
+					ParseMode: "markdownv2"}})
+			_, err := inlq.Answer(b,
+				results,
+				&gotgbot.AnswerInlineQueryOpts{IsPersonal: true})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for i := range l {
+		txt = txt.AppendItalic(l[i]).AppendNormal("\n")
+	}
+
+	results = append(results, gotgbot.InlineQueryResultArticle{Id: inlq.Id, Title: fmt.Sprintf("Lyrics: %s", q),
+		InputMessageContent: gotgbot.InputTextMessageContent{MessageText: txt.ToString(),
+			ParseMode: "markdownv2"}})
+	_, err = inlq.Answer(b,
+		results,
+		&gotgbot.AnswerInlineQueryOpts{IsPersonal: true})
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
 func lyricsHandler(b *gotgbot.Bot, ctx *ext.Context) (err error) {
 	msg := ctx.EffectiveMessage
 	args := ctx.Args()
