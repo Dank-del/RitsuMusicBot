c82ac85bd80d74ea918ac116aed8858fd87b1af8
Sayan Biswas <sayan@pokurt.me>
2021-09-14 01:00:35 +0530

    new: test for odesli
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

---
+++ tests/odesli_test.go
@@ -0,0 +1,18 @@
+package tests
+
+import (
+	"gitlab.com/Dank-del/lastfm-tgbot/odesli"
+	"testing"
+)
+
+func TestOdesliByUrl(t *testing.T) {
+	r, err := odesli.GetLinks("https://open.spotify.com/track/7iYMW4Ww40RAbfeccmzMQa?si=069155796edc4a61")
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+
+	t.Log(r.EntitiesByUniqueID[r.EntityUniqueID].Title)
+	t.Log(r.LinksByPlatform.Tidal)
+
+}
