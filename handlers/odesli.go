69bb7d7da9e08345b2ede71ddc673cabc4e56084
Sayan Biswas <sayan@pokurt.me>
2021-09-14 15:09:08 +0530

    odesli: change message style
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- handlers/odesli.go
+++ handlers/odesli.go
@@ -34,51 +34,55 @@ func odesliMessageHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 		logging.Error(d.Code)
 		return errors.New(d.Code)
 	}
-	txt := mdparser.GetUserMention(msg.From.FirstName, msg.From.Id).AppendBold(" sent").AppendNormal("\n")
+	txt := mdparser.GetUserMention(msg.From.FirstName, msg.From.Id).AppendBold(" sent").AppendNormal("\n\n")
 	t := d.EntitiesByUniqueID[d.EntityUniqueID]
-	txt = txt.AppendItalic(t.ArtistName).AppendNormal(" - ").AppendBold(t.Title).AppendNormal("\n")
+	txt = txt.AppendItalic(t.ArtistName).AppendNormal(" - ").AppendBold(t.Title).AppendNormal("\n\n")
 	links := d.LinksByPlatform
+	dot := "• "
+	newline := "\n"
 	if links.Deezer != nil {
-		txt = txt.AppendHyperLink("Deezer", links.Deezer.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Deezer", links.Deezer.URL).AppendNormal(newline)
 	}
 	if links.Itunes != nil {
-		txt = txt.AppendHyperLink("Itunes", links.Itunes.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Itunes", links.Itunes.URL).AppendNormal(newline)
 	}
 	if links.Tidal != nil {
-		txt = txt.AppendHyperLink("Tidal", links.Deezer.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Tidal", links.Deezer.URL).AppendNormal(newline)
 	}
 	if links.AmazonMusic != nil {
-		txt = txt.AppendHyperLink("Amazon Music", links.AmazonMusic.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Amazon Music", links.AmazonMusic.URL).AppendNormal(newline)
 	}
 	if links.AmazonStore != nil {
-		txt = txt.AppendHyperLink("Amazon Store", links.AmazonStore.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Amazon Store", links.AmazonStore.URL).AppendNormal(newline)
 	}
 	if links.Napster != nil {
-		txt = txt.AppendHyperLink("Napster", links.Napster.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Napster", links.Napster.URL).AppendNormal(newline)
 	}
 	if links.AppleMusic != nil {
-		txt = txt.AppendHyperLink("Apple Music", links.AppleMusic.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Apple Music", links.AppleMusic.URL).AppendNormal(newline)
 	}
 	if links.Pandora != nil {
-		txt = txt.AppendHyperLink("Pandora", links.Pandora.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Pandora", links.Pandora.URL).AppendNormal(newline)
 	}
 	if links.Youtube != nil {
-		txt = txt.AppendHyperLink("Youtube", links.Youtube.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Youtube", links.Youtube.URL).AppendNormal(newline)
 	}
 	if links.Spotify != nil {
-		txt = txt.AppendHyperLink("Spotify", links.Spotify.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Spotify", links.Spotify.URL).AppendNormal(newline)
 	}
 	if links.Soundcloud != nil {
-		txt = txt.AppendHyperLink("SoundCloud", links.Soundcloud.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("SoundCloud", links.Soundcloud.URL).AppendNormal(newline)
 	}
 	if links.Yandex != nil {
-		txt = txt.AppendHyperLink("Yandex", links.Yandex.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Yandex", links.Yandex.URL).AppendNormal(newline)
 	}
 	if links.YoutubeMusic != nil {
-		txt = txt.AppendHyperLink("Youtube Music", links.YoutubeMusic.URL).AppendNormal(" ")
+		txt = txt.AppendNormal(dot).AppendHyperLink("Youtube Music", links.YoutubeMusic.URL).AppendNormal(newline)
 	}
+
+	txt = txt.AppendNormal(newline + newline).AppendItalic("Powered by Odesli smartlink API")
 	// print(txt.ToString())
-	_, err = msg.Reply(b, txt.ToString(), &gotgbot.SendMessageOpts{ParseMode: "markdownv2"})
+	_, err = msg.Reply(b, txt.ToString(), &gotgbot.SendMessageOpts{ParseMode: "markdownv2", DisableWebPagePreview: true})
 	if err != nil {
 		logging.Error(err.Error())
 		return err
