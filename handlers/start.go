dda28a2aed9f7bf056369d7d38bba1b68f76f277
Sayan Biswas <me@sayanbiswas.in>
2022-02-13 20:38:02 +0530

    start: use token map for authentication
    Signed-off-by: Sayan Biswas <sayan@intellivoid.net>

--- handlers/start.go
+++ handlers/start.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/ALiwoto/StrongStringGo/strongStringGo"
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 	"github.com/zmb3/spotify/v2"
 	auth2 "gitlab.com/Dank-del/lastfm-tgbot/core/auth"
+	"gitlab.com/Dank-del/lastfm-tgbot/database"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"html"
@@ -21,13 +23,20 @@ func startHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 	if chat.Type == "private" {
 		ctxBg := context.Background()
 		if len(args) > 1 && strings.HasPrefix(args[1], "spotifyCode") {
-			code := strings.ReplaceAll(args[1], "spotifyCode", "")
+			auth2.TokenMutex.RLock()
+			defer auth2.TokenMutex.RUnlock()
+			code := auth2.TokenMap[strings.ReplaceAll(args[1], "spotifyCode", "")]
+			if code == strongStringGo.EMPTY {
+				return ext.EndGroups
+			}
 			auth := spotify.New(auth2.SpotifyAuthenticator.Client(ctxBg, &oauth2.Token{RefreshToken: code}))
 			currentUser, err := auth.CurrentUser(ctxBg)
 			if err != nil {
-				return err
+				message = err.Error()
+			} else {
+				database.UpdateSpotifyUser(user.Id, code)
+				message = fmt.Sprintf("Logged in as %s (%s)", currentUser.DisplayName, currentUser.Product)
 			}
-			message = fmt.Sprintf("Logged in as %s (%s)", currentUser.DisplayName, currentUser.Product)
 		} else {
 			message = fmt.Sprintf("<b>Hi %s, I'm %s</b>!\n<i>Run /help to learn more!</i>",
 				html.EscapeString(user.FirstName),
